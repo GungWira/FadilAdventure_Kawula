7b1b33ee5b4395b3e5b286371571af946141acda
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2025-04-26 20:23:34 +0700

    push episode chapter

--- app/api/chapters/many/route.ts
+++ app/api/chapters/many/route.ts
@@ -4,18 +4,35 @@ import { createErrorResponse, handleServerError } from "../../error/error-handle
 
 export async function GET(): Promise<NextResponse> {
   try {
-    const { data, error } = await supabase
+    const { data: chaptersData, error } = await supabase
       .from("chapters")
       .select("*");
 
     if (error) {
       return createErrorResponse(error.details, 400);
     }
-    if (!data) {
+    if (!chaptersData) {
       return createErrorResponse("Chapters not found", 404);
     }
+
+    const episodesIds = chaptersData.flatMap(chapter => chapter.episodes_id);
+    const { data: episodes, error: episodesError } = await supabase
+      .from("episodes")
+      .select("*")
+      .in("id", episodesIds);
+
+    if (episodesError) {
+      return createErrorResponse(episodesError.details, 400);
+    }
+
+    const resultData = chaptersData.map(chapter => ({
+      ...chapter,
+      episodes: episodes?.filter(episode => 
+        chapter.episodes_id.includes(episode.id)
+      )
+    }));
     
-    return NextResponse.json(data, { status: 200 });
+    return NextResponse.json(resultData, { status: 200 });
   } catch (error) {
     return handleServerError(error);
   }
