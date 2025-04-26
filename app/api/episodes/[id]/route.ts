093b95ff622e0cbd2947628987c338b0132c3ad0
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2025-04-26 22:21:45 +0700

    fixing question

---
+++ app/api/episodes/[id]/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "../../supa-client";
+import {
+  createErrorResponse,
+  handleServerError,
+} from "../../error/error-handle";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+): Promise<NextResponse> {
+  try {
+    const episodeId = (await params).id;
+
+    console.log(episodeId, "episodeId");
+
+    if (!episodeId) {
+      return createErrorResponse("Episode ID is required", 400);
+    }
+    
+    const { data: episode_questions, error: episodeQuestionsError } = await supabase
+      .from("episodes")
+      .select("questions_id")
+      .eq("id", episodeId)
+      .single();
+
+    if (episodeQuestionsError) {
+      return createErrorResponse(episodeQuestionsError.message, 500);
+    }
+
+    const { data: questions, error: questionsError } = await supabase
+      .from("questions")
+      .select("id, question, answer, answer_options")
+      .in("id", episode_questions.questions_id || []);
+
+    console.log(questions)
+    
+    const flattenedQuestions = questions?.flatMap(q => ({
+      id: q.id,
+      question: q.question,
+      answer: q.answer,
+      options: q.answer_options
+    })) || [];
+
+    if (questionsError) {
+      return createErrorResponse(questionsError.message, 500);
+    } 
+
+    return NextResponse.json(flattenedQuestions);
+  } catch (error) {
+    return handleServerError(error);
+  }
+}
