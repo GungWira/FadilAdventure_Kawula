8b2c03cd2e3814ac5ab517d1c7d020046f5c4594
moshcat <mauricecathoverile@gmail.com>
2025-04-26 11:51:03 +0800

    chore: initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Progress } from "@/components/ui/progress"
+import { Button } from '@/components/ui/button'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+  
+
+export default function test() {
+  return (
+    <div>
+        <Progress value={20} />
+        <Drawer>
+  <DrawerTrigger>Open</DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+    </DrawerHeader>
+    <DrawerFooter>
+      <Button>Submit</Button>
+      <DrawerClose>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
+
+    </div>
+  )
+}
