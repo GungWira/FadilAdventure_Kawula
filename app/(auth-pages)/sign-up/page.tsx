1981a943bd20ebc6327391b2d44f66c5f3deebe6
GungWira <wiradarma002@gmail.com>
2025-04-26 12:44:37 +0700

    Redesign ui for signup

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
+import Image from "next/image";
 
 export default async function Signup(props: {
   searchParams: Promise<Message>;
@@ -20,32 +21,50 @@ export default async function Signup(props: {
 
   return (
     <>
-      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?{" "}
-          <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
-          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
-            Sign up
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-      <SmtpMessage />
+      <div className="w-screen h-screen fixed top-0 left-0 bg-[#7E80D8] flex justify-center items-center">
+        <form className=" relative z-10 flex flex-col mx-auto bg-white w-full max-w-96 px-8 py-8 rounded-xl">
+          <div className="flex flex-col justify-center items-center gap-1">
+            <Image
+              src={"/logo-w-bg.svg"}
+              alt="Logo"
+              width={40}
+              height={40}
+              className="w-16 aspect-square mb-2"
+            />
+            <p className="text-sm text text-foreground">
+              Yuk gabung bareng kawula
+            </p>
+            <h1 className="text-2xl font-medium my-1">Buat Akun</h1>
+            <div className="flex justify-center items-center gap-2">
+              <div className="w-3 aspect-square rounded-full bg-[#7E80D8]"></div>
+              <p className="text-sm text text-foreground">11.301 Anggota</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" placeholder="you@example.com" required />
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              minLength={6}
+              required
+            />
+            <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+              Buat Akun
+            </SubmitButton>
+            <FormMessage message={searchParams} />
+          </div>
+        </form>
+        <Image
+          src={"/bg-signup.webp"}
+          alt="background"
+          width={1440}
+          height={1440}
+          className="w-full absolute"
+        />
+      </div>
     </>
   );
 }
